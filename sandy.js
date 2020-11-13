d99a7fd0d97474d1be509fd7564f40593e097810
jdlevish <45375460+jdlevish@users.noreply.github.com>
2020-11-13 15:58:18 -0800

    changed nav font and some other layout issues

--- sandy.js
+++ sandy.js
@@ -1,24 +1,22 @@
 
-$("#scroller").simplyScroll();
 
-var slideIndex = 0;
+
+
+var count = 0;
 showSlides();
-
 function showSlides() {
-  var i;
-  var slides = document.getElementsById("img-id");
-  var dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+  var slides = document.getElementsByClassName("dot");
+  console.log(slides.length)
+  for (let i = 0; i < slides.length; i++) {
+    console.log(slides[i])
+    slides[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
+  count++
+  if (count >= slides.length) { count = 0 };
+
+  slides[count].style = "display: grid";
+  setTimeout(showSlides, 5000);
 }
+
 
 
