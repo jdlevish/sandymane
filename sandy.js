81fb37999b96d8f76bab7217ec5081adae681d3f
jdlevish <45375460+jdlevish@users.noreply.github.com>
2020-11-16 17:01:50 -0800

    fixed centering issue when in mobile view, moved logo to center on mobile view as well

--- sandy.js
+++ sandy.js
@@ -6,9 +6,9 @@ var count = 0;
 showSlides();
 function showSlides() {
   var slides = document.getElementsByClassName("dot");
-  console.log(slides.length)
+
   for (let i = 0; i < slides.length; i++) {
-    console.log(slides[i])
+
     slides[i].style.display = "none";
   }
   count++
